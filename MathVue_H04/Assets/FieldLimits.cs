21c76d99d248509ef382551dafe731786b00c43a
MohanedB <mohanedbouzaidi@gmail.com>
2025-03-11 18:29:20 -0400

    Started working, I have to do the puck spawner now
    Started working, I have to do the puck spawner now

---
+++ MathVue_H04/Assets/FieldLimits.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldLimits : MonoBehaviour
+{
+    public float xMin = -5f;
+    public float xMax = 5f;
+    public float zMin = -3f;
+    public float zMax = 3f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
